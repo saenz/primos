0b470b4674121b8d7a4504a7d7ea8f78e352ab7e
jeff saenz <jsaenzsweng@gmail.com>
2018-05-18 10:57:35 -0700

    Deploy Next.js to primos

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "f34d48cc828b29e5e0d9"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "8b0b88f05c1b8cddf7ce"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","4":"bundles/pages/index.js","5":"bundles/pages/about.js","6":"bundles/pages/contact.js","7":"bundles/pages/login.js"}[chunkId]||chunkId) + "-" + {"0":"e739e27202ee9737a2da","1":"5bef41eb30202310ec0a","2":"5d4fb4e0d6ac5dd63490","4":"721a2e28fae49b604f2a","5":"706b37ba89527ed8700a","6":"b203f0a0d34e21ffe3dc","7":"c03b6782af82ca046e5d"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_app.js","4":"bundles/pages/contact.js","5":"bundles/pages/about.js"}[chunkId]||chunkId) + "-" + {"0":"e739e27202ee9737a2da","1":"5bef41eb30202310ec0a","2":"5d4fb4e0d6ac5dd63490","4":"fdcb57bf9de7c978bdae","5":"706b37ba89527ed8700a"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
